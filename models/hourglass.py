fec0bde4729d881793a54f08ee854abdf704f87d
Desktop <dorndorf8@gmail.com>
2026-01-04 14:31:50 -0500

    Add initial residual block

--- models/hourglass.py
+++ models/hourglass.py
@@ -2,15 +2,21 @@ import torch
 import torch.nn as nn
 
 class ResidualModule(nn.Module):
-    def __init__(self):
+    def __init__(self, C_in=256, C_mid=128, C_out=256):
         super(ResidualModule, self).__init__()
 
         self.block = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=1, padding=1)
+            nn.BatchNorm2d(C_in)
             ,nn.GELU()
-            ,nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+            ,nn.Conv2d(C_in, C_mid, kernel_size=1, padding=1)
+
+            ,nn.BatchNorm2d(C_mid)
             ,nn.GELU()
-            ,nn.Conv2d(in_channels=128, out_channels=256, kernel_size=1, padding=1)
+            ,nn.Conv2d(C_mid, C_mid, kernel_size=3, padding=1)
+
+            ,nn.BatchNorm2d(C_mid)
+            ,nn.GELU()
+            ,nn.Conv2d(C_mid, C_out, kernel_size=1, padding=1)
         )
 
     def forward(self, x):
