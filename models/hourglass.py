8a1c7dc60dc59e56f1bce2e3cf20b9a47cd95a25
Desktop <dorndorf8@gmail.com>
2026-01-04 14:36:13 -0500

    Add initial LayerNorm2d and ability to select normalization in residual blocks

--- models/hourglass.py
+++ models/hourglass.py
@@ -1,20 +1,22 @@
 import torch
 import torch.nn as nn
+import utils
+from Typing import Callable
 
 class ResidualModule(nn.Module):
-    def __init__(self, C_in=256, C_mid=128, C_out=256):
+    def __init__(self, C_in:int=256, C_mid:int=128, C_out:int=256, normalization: Callable=nn.BatchNorm2d):
         super(ResidualModule, self).__init__()
 
         self.block = nn.Sequential(
-            nn.BatchNorm2d(C_in)
+            normalization(C_in)
             ,nn.GELU()
             ,nn.Conv2d(C_in, C_mid, kernel_size=1, padding=1)
 
-            ,nn.BatchNorm2d(C_mid)
+            ,normalization(C_mid)
             ,nn.GELU()
             ,nn.Conv2d(C_mid, C_mid, kernel_size=3, padding=1)
 
-            ,nn.BatchNorm2d(C_mid)
+            ,normalization(C_mid)
             ,nn.GELU()
             ,nn.Conv2d(C_mid, C_out, kernel_size=1, padding=1)
         )
---
+++ models/utils.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class LayerNorm2d(nn.Module):
+    def __init__(self, channels, eps=1e-6):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(channels))
+        self.bias = nn.Parameter(torch.zeros(channels))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(1, keepdim=True)
+        var = (x - mean).pow(2).mean(1, keepdim=True)
+        x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.weight[:, None, None] * x + self.bias[:, None, None]
