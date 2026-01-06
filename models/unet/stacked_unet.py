d2198910952812eb0adafcf3735dc6866da1c53d
orndoda_mac <dorndorf8@gmail.com>
2026-01-05 19:49:35 -0500

    Stacked unet finished

--- models/unet/stacked_unet.py
+++ models/unet/stacked_unet.py
@@ -10,30 +10,26 @@ from models.utils import DownSample
 from .residual_module import *
 
 class IntermediateResidualModule(nn.Module):
-    def __init__(self, normalization: Callable = nn.BatchNorm2d, intermdiate_super: bool = True):
+    def __init__(self, normalization: Callable = nn.BatchNorm2d):
         super(IntermediateResidualModule, self).__init__()
         self.normalization = normalization
-        self.intermdiate_super = intermdiate_super
 
         self.res1 = ResidualModule(64, 32, 64, normalization=normalization)
         self.res2 = ResidualModule(64, 32, 64, normalization=normalization)
-        if self.intermdiate_super:
-            self.res3 = ResidualModule(64, 32, 64, normalization=normalization)
+
+        self.res3 = ResidualModule(64, 32, 64, normalization=normalization)
 
     def forward(self, x):
         x = self.res1(x)
+        inter_out = x
         x = self.res2(x)
 
-        if self.intermdiate_super:
-            return x, self.res3(x)
-
-        return x
+        return x + self.res3(inter_out), inter_out
 
 class StackedUnet(nn.Module):
     def __init__(self, in_channels: int=3, normalization: Callable=nn.BatchNorm2d,
                  inner_module: int=1,
                  interpolation_mode: Literal["bilinear","bicubic"]="bilinear",
-                 intermediate_super: bool = True,
         ):
         super(StackedUnet, self).__init__()
 
@@ -41,18 +37,17 @@ class StackedUnet(nn.Module):
         self.inner_module = inner_module
         self.interpolation_mode = interpolation_mode
         self.in_channels = in_channels
-        self.intermediate_super = intermediate_super
 
         self.downsample = DownSample(self.normalization)
 
         self.unet_stack1 = UnetModule(self.normalization, self.inner_module)
-        self.inter1 = IntermediateResidualModule(self.normalization, self.intermediate_super)
+        self.inter1 = IntermediateResidualModule(self.normalization)
 
         self.unet_stack2 = UnetModule(self.normalization, self.inner_module)
-        self.inter2 = IntermediateResidualModule(self.normalization, self.intermediate_super)
+        self.inter2 = IntermediateResidualModule(self.normalization)
 
         self.unet_stack3 = UnetModule(self.normalization, self.inner_module)
-        self.inter3 = IntermediateResidualModule(self.normalization, self.intermediate_super)
+        self.inter3 = IntermediateResidualModule(self.normalization)
 
     def forward(self, x):
         x = F.interpolate(x,
@@ -62,25 +57,16 @@ class StackedUnet(nn.Module):
 
         x = self.downsample(x)
 
-        x = self.unet_stack1(x)
-        if self.intermediate_super:
-            x, out1 = self.inter1(x)
-        else:
-            x = self.inter1(x)
+        u1 = self.unet_stack1(x)
+        x, out1 = self.inter1(u1)
+        x = u1 + x
 
-        x = self.unet_stack2(x)
-        if self.intermediate_super:
-            x, out2 = self.inter2(x)
-        else:
-            x = self.inter2(x)
+        u2 = self.unet_stack1(x)
+        x, out2 = self.inter1(u2)
+        x = u2 + x
 
-        x = self.unet_stack3(x)
-        if self.intermediate_super:
-            x, out3 = self.inter3(x)
-        else:
-            x = self.inter3(x)
+        u3 = self.unet_stack1(x)
+        x, out3 = self.inter1(u3)
+        x = u3 + x
 
-        if self.intermediate_super:
-            return x, [out1, out2, out3]
-
-        return x, []
+        return x, [out1, out2, out3]
